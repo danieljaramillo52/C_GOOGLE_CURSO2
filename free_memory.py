2ae92ab0be29ea9264b2d260581899607f368351
daniel.jaramillo24 <daniel.jaramillo24@udea.edu.co>
2025-07-16 12:28:07 -0500

    Cambio en la función suma elementos

--- free_memory.py
+++ free_memory.py
@@ -6,3 +6,12 @@ def main():
         print("Todo está Ok!")
 
 main()
+
+def suma_elementos(num1: int, num2: int):
+        """Suma dos elementos
+        
+        Args:
+            num1 (int): primer elemento a sumar
+            num2 (int): segundo número a sumar
+        """
+        return num1 + num2
